3064a5d0ba58080eb4d706642776dc85b6f7bb61
Tauseef Shoaib <tauseefshoaib@gmail.com>
2026-04-23 15:58:25 +0530

    add routes and validation

--- src/app.js
+++ src/app.js
@@ -3,16 +3,44 @@ const connectDB = require("./config/db");
 const User = require("./models/user");
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Virat",
-    lastName: "Kohli",
-    email: "viratkohli@gmail.com",
-    password: "virat@123",
-  });
+  const user = new User(req.body);
 
   await user.save();
   res.send("User added successfully!");
+});
+
+// get all users
+app.get("/feed", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+
+// find a user
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  const user = await User.find({ emailId: userEmail });
+  res.send(user);
+});
+
+// delete a user
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  await User.findByIdAndDelete(userId);
+  res.send("User deleted successfully!");
+});
+
+// update a user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  await User.findByIdAndUpdate(userId, data);
+  res.send("User updated successfully!");
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,47 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+      type: String,
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+);
 
 const User = mongoose.model("User", userSchema);
 
